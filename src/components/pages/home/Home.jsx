30c70d67761f4ee22db249b247a87fe689ea2267
Mauricio Abregú <mauricio.matias93@gmail.com>
2023-06-23 08:44:28 -0300

    Modificaciones en el home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => {
+    setContador(contador + 1);
+  };
   return (
-    <div>
+    <div style={{ padding: "0 20px" }}>
       <h1>Estoy en el Home</h1>
+      <h2>{contador}</h2>
+      <button onClick={sumar}>Sumar</button>
     </div>
   );
 };
